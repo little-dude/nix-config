d29cff170c42d76b6a7997acf1203c6a47765f0f
little-dude <little-dude@gmail.com>
2024-08-03 11:42:18 +0200

    install system76 tools

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -40,4 +40,5 @@
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode =
     lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.system76.enableAll = true;
 }
