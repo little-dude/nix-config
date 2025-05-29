35a396d5c38daf625d63ef2da158bae4c6a7c1e3
little-dude <corentinhenry@gmail.com>
2025-05-29 16:31:10 +0200

    Revert "configure gpu passthrough"
    This reverts commit bf3a4e65339a0d3899cdb3918dad2f46b53355be.

--- nixos/virtualization.nix
+++ nixos/virtualization.nix
@@ -1,43 +1,13 @@
 { config, pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    virt-manager
-    qemu
-    OVMF
-    pciutils
-  ];
+  environment.systemPackages = [ pkgs.virt-manager ];
   virtualisation = {
-    libvirtd = {
-      enable = true;
-      qemuPackage = pkgs.qemu_kvm;
-      qemuVerbatimConfig = ''
-        nvram = [
-        "${pkgs.OVMF}/FV/OVMF.fd:${pkgs.OVMF}/FV/OVMF_VARS.fd"
-        ]
-      '';
-    };
+    libvirtd.enable = true;
     docker.enable = true;
   };
-  users.groups = {
-    libvirtd.members = [
-      "root"
-      "little-dude"
-    ];
-    docker.members = [
-      "root"
-      "little-dude"
-    ];
-  };
-  boot = {
-    # These modules are required for PCI passthrough, and must come before early modesetting stuff
-    kernelModules = [
-      "kvm-intel"
-      "vfio"
-      "vfio_iommu_type1"
-      "vfio_pci"
-      "vfio_virqfd"
-    ];
-    kernelParams = [ "intel_iommu=on" ];
-    extraModprobeConfig = "options vfio-pci ids=[10de:28e0, 10de:22be]";
-  };
+  users.users.little-dude.extraGroups = [
+    "docker"
+    "libvirtd"
+  ];
+  boot.kernelModules = [ "kvm-intel" ];
 }
