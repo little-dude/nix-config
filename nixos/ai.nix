fe765de9e4332ed7f024ee5228f868d96966b5e0
little-dude <corentinhenry@gmail.com>
2025-05-29 16:31:10 +0200

    add AI module
    note that oterm doesn't build atm

---
+++ nixos/ai.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }:
+{
+  services.ollama = {
+    enable = true;
+    acceleration = "cuda";
+  };
+  # environment.systemPackages = [ pkgs.oterm ];
+  services.open-webui = {
+    package = pkgs.open-webui;
+    enable = true;
+    environment = {
+      ANONYMIZED_TELEMETRY = "False";
+      DO_NOT_TRACK = "True";
+      SCARF_NO_ANALYTICS = "True";
+      OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api";
+      OLLAMA_BASE_URL = "http://127.0.0.1:11434";
+    };
+  };
+}
