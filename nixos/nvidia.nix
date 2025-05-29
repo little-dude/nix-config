48acb59dddc8ef458d3932ee62712d01aa0614b7
little-dude <corentinhenry@gmail.com>
2025-05-29 16:31:10 +0200

    configure gpu passthrough

--- nixos/nvidia.nix
+++ nixos/nvidia.nix
@@ -1,55 +1,55 @@
 { config, ... }:
 {
   # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = [ "nvidia" ];
+  # services.xserver.videoDrivers = [ "nvidia" ];
 
-  hardware = {
-    graphics = {
-      enable = true;
-      enable32Bit = true;
-    };
-    nvidia = {
-      # Modesetting is required.
-      modesetting.enable = true;
+  # hardware = {
+  #   graphics = {
+  #     enable = true;
+  #     enable32Bit = true;
+  #   };
+  #   nvidia = {
+  #     # Modesetting is required.
+  #     modesetting.enable = true;
 
-      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-      powerManagement = {
-        enable = false;
-        # Fine-grained power management. Turns off GPU when not in use.
-        # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-        finegrained = false;
-      };
+  #     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+  #     powerManagement = {
+  #       enable = false;
+  #       # Fine-grained power management. Turns off GPU when not in use.
+  #       # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+  #       finegrained = false;
+  #     };
 
-      # Use the NVidia open source kernel module (not to be confused with the
-      # independent third-party "nouveau" open source driver).
-      # Support is limited to the Turing and later architectures. Full list of
-      # supported GPUs is at:
-      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-      # Only available from driver 515.43.04+
-      # Do not disable this unless your GPU is unsupported or if you have a good reason to.
-      open = true;
+  #     # Use the NVidia open source kernel module (not to be confused with the
+  #     # independent third-party "nouveau" open source driver).
+  #     # Support is limited to the Turing and later architectures. Full list of
+  #     # supported GPUs is at:
+  #     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+  #     # Only available from driver 515.43.04+
+  #     # Do not disable this unless your GPU is unsupported or if you have a good reason to.
+  #     open = true;
 
-      # Enable the Nvidia settings menu,
-      # accessible via `nvidia-settings`.
-      nvidiaSettings = true;
+  #     # Enable the Nvidia settings menu,
+  #     # accessible via `nvidia-settings`.
+  #     nvidiaSettings = true;
 
-      # We have a GeForce RTX 4060 which is not in the legacy GPU list (https://www.nvidia.com/en-us/drivers/unix/legacy-gpu/)
-      # Therefore, the stable drivers should work.
-      #
-      # [little-dude@nixos:~]$ nix-shell -p pciutils --run "lspci -nn | grep VGA"
-      # 00:02.0 VGA compatible controller [0300]: Intel Corporation Raptor Lake-S UHD Graphics [8086:a788] (rev 04)
-      # 01:00.0 VGA compatible controller [0300]: NVIDIA Corporation AD107M [GeForce RTX 4060 Max-Q / Mobile] [10de:28e0] (rev a1)
-      package = config.boot.kernelPackages.nvidiaPackages.production;
+  #     # We have a GeForce RTX 4060 which is not in the legacy GPU list (https://www.nvidia.com/en-us/drivers/unix/legacy-gpu/)
+  #     # Therefore, the stable drivers should work.
+  #     #
+  #     # [little-dude@nixos:~]$ nix-shell -p pciutils --run "lspci -nn | grep VGA"
+  #     # 00:02.0 VGA compatible controller [0300]: Intel Corporation Raptor Lake-S UHD Graphics [8086:a788] (rev 04)
+  #     # 01:00.0 VGA compatible controller [0300]: NVIDIA Corporation AD107M [GeForce RTX 4060 Max-Q / Mobile] [10de:28e0] (rev a1)
+  #     package = config.boot.kernelPackages.nvidiaPackages.production;
 
-      prime = {
-        sync.enable = true;
-        #   offload = {
-        #     enable = true;
-        #     enableOffloadCmd = true;
-        #   };
-        intelBusId = "PCI:0:2:0";
-        nvidiaBusId = "PCI:1:0:0";
-      };
-    };
-  };
+  #     prime = {
+  #       sync.enable = true;
+  #       #   offload = {
+  #       #     enable = true;
+  #       #     enableOffloadCmd = true;
+  #       #   };
+  #       intelBusId = "PCI:0:2:0";
+  #       nvidiaBusId = "PCI:1:0:0";
+  #     };
+  #   };
+  # };
 }
