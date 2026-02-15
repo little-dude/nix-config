1c0fccdce70d9453ce3e8a90c3ae0c7130a25d36
little-dude <corentinhenry@gmail.com>
2026-02-15 16:20:51 +0100

    enable nvidia GPU in docker

--- nixos/virtualization.nix
+++ nixos/virtualization.nix
@@ -4,6 +4,7 @@
     libvirtd.enable = true;
     docker.enable = true;
   };
+  hardware.nvidia-container-toolkit.enable = true;
   users.users.little-dude.extraGroups = [
     "docker"
     "libvirtd"
